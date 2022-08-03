ea734b5e9b2426bc7f1e125ee5954693504b5b9b
Džuris <juris@glaive.pro>
2022-08-03 20:08:38 +0300

    Document facade

--- src/GPImage.php
+++ src/GPImage.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \GlaivePro\Image\Uri url(string $path, int $width = null, int $height = null) Get a stringable image Uri and optionally set size.
+ * @method static \GlaivePro\Image\Uri asset(string $path, int $width = null, int $height = null) Get a stringable image Url and optionally set size.
+ * @method static void filter(string $key, callable $filter) Register a custom handler.
  *
  * @see \GlaivePro\Image\Image
  */
