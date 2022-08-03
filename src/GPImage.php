c5f56476d6587a3a84c54331ae76d244e1f9891c
Džuris <juris@glaive.pro>
2022-08-03 12:32:27 +0300

    Document th facade

--- src/GPImage.php
+++ src/GPImage.php
@@ -4,6 +4,11 @@ namespace GlaivePro\Image;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \GlaivePro\Image\Uri url(string $path, int $width = null, int $height = null) Get a stringable image Uri and optionally set size.
+ *
+ * @see \GlaivePro\Image\Image
+ */
 class GPImage extends Facade
 {
 	protected static function getFacadeAccessor(): string
