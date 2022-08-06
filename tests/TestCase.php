cd8d0e63bafa516e854875ade2f24315c8d1ed88
tontonsb <tontonsb@users.noreply.github.com>
2022-08-06 12:55:22 +0000

    Apply CS fixes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -45,41 +45,41 @@ abstract class TestCase extends OrchestraTestCase
 	}
 
 	/**
-     * For compatibility with Laravel 8 and Laravel 9-prefer-lowest.
+	 * For compatibility with Laravel 8 and Laravel 9-prefer-lowest.
 	 * Taken directly from:
 	 * https://github.com/laravel/framework/blob/87d1da43ca53cd59f00df552466c7540fffeab25/src/Illuminate/Foundation/Testing/Concerns/InteractsWithExceptionHandling.php#L161
-     */
-    protected function assertThrows(\Closure $test, string $expectedClass = \Throwable::class, ?string $expectedMessage = null): self
-    {
-        try {
-            $test();
+	 */
+	protected function assertThrows(\Closure $test, string $expectedClass = \Throwable::class, ?string $expectedMessage = null): self
+	{
+		try {
+			$test();
 
-            $thrown = false;
-        } catch (\Throwable $exception) {
-            $thrown = $exception instanceof $expectedClass;
+			$thrown = false;
+		} catch (\Throwable $exception) {
+			$thrown = $exception instanceof $expectedClass;
 
-            $actualMessage = $exception->getMessage();
-        }
+			$actualMessage = $exception->getMessage();
+		}
 
-        Assert::assertTrue(
-            $thrown,
-            sprintf('Failed asserting that exception of type "%s" was thrown.', $expectedClass)
-        );
+		Assert::assertTrue(
+			$thrown,
+			sprintf('Failed asserting that exception of type "%s" was thrown.', $expectedClass)
+		);
 
-        if (isset($expectedMessage)) {
-            if (! isset($actualMessage)) {
-                Assert::fail(
-                    sprintf(
-                        'Failed asserting that exception of type "%s" with message "%s" was thrown.',
-                        $expectedClass,
-                        $expectedMessage
-                    )
-                );
-            } else {
-                Assert::assertStringContainsString($expectedMessage, $actualMessage);
-            }
-        }
+		if (isset($expectedMessage)) {
+			if (! isset($actualMessage)) {
+				Assert::fail(
+					sprintf(
+						'Failed asserting that exception of type "%s" with message "%s" was thrown.',
+						$expectedClass,
+						$expectedMessage
+					)
+				);
+			} else {
+				Assert::assertStringContainsString($expectedMessage, $actualMessage);
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 }
