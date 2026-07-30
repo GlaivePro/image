a725c23674c0ba4a296045a2761582543cb3e9d2
Džuris <juris@glaive.pro>
2026-07-30 21:12:21 +0300

    Switch CS tooling

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,40 @@
 <?php
 
-return JanaSeta\PhpCs\Fix::in([
-	'src',
-	'tests',
-])->addRules([
-	'void_return' => false,
-]);
+$finder = PhpCsFixer\Finder::create()
+    ->in([
+        'src',
+        'tests',
+    ]);
+
+return (new PhpCsFixer\Config)->setFinder($finder)
+	->setRiskyAllowed(true)
+	->setLineEnding("\n")
+    /**
+     * No style rules, just some cleanliness.
+     */
+	->setRules([
+		'@PHP8x5Migration:risky' => true,
+		'@PHP8x5Migration' => true,
+
+		// Overrides `@PHP8x5Migration:risky`
+		'declare_strict_types' => false,
+        'void_return' => false,
+
+		// Overrides `@PHP8x5Migration`
+        'method_argument_space' => false,
+
+		// Additional
+        'combine_consecutive_issets' => true,
+        'modernize_types_casting' => true,
+        'native_function_casing' => true,
+        'no_alias_functions' => true,
+        'no_empty_statement' => true,
+        'no_superfluous_elseif' => true,
+        'no_unreachable_default_argument_value' => true,
+        'no_unneeded_import_alias' => true,
+        'no_unused_imports' => true,
+        'no_useless_concat_operator' => true,
+        'no_useless_else' => true,
+        'no_useless_return' => true,
+        'no_useless_sprintf' => true,
+	]);
